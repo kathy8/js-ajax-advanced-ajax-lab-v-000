c4c9af28a9d62d07c60a5a9ed3bad4cb7f9e531c
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-06-14 06:10:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-document.addEventListener("DOMContentLoaded",
-  function(event) {
-    Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
-  });
+document.addEventListener("DOMContentLoaded", function(event) {
+ Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
+});
 
 function getRepositories() {
-  const req = new XMLHttpRequest()
+  const req = new XMLHttpRequest();
   req.addEventListener("load", showRepositories);
   req.open("GET", 'https://api.github.com/users/octocat/repos');
   req.send();
@@ -14,6 +13,7 @@ function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText);
   const src = document.getElementById("repository-template").innerHTML;
   const template = Handlebars.compile(src);
-  const repoList = template(repos)
+  const repoList = template(repos);
+
   document.getElementById("repositories").innerHTML = repoList;
 }
